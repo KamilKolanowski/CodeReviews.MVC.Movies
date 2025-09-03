1264eb93329756bb681e680c5f8fb018a2aaf157
Kamil Kolanowski <tanssive@yahoo.com>
2025-09-03 12:42:44 +0200

    final ver

---
+++ MVC.Movies.KamilKolanowski/Migrations/20250903102204_Initial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC.Movies.KamilKolanowski.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movies");
+        }
+    }
+}
